dc49aba44ec1d137991ba5098737c660a2bdc17e
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-06-01 19:18:28 +0200

    Test connection to google calendar api successfuly

---
+++ run.py
@@ -0,0 +1,33 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+# from datetime import datetime, date, timedelta
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/calendar.readonly"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPE_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SHEET = GSPREAD_CLIENT.open('working-hours-reports')
+CALENDAR_SERVICE = build('calendar', 'v3', credentials=SCOPE_CREDS)
+
+
+def fetch_calendar():
+    """
+    test function to check API connection and fetch calendar info
+    """
+    calendar_id = input("Please enter your calendar ID:\n")
+    try:
+        calendar_info = CALENDAR_SERVICE.calendars().get(calendarId=calendar_id).execute()
+        print("Calendar connected successfully!")
+        print("Calendar name: ", calendar_info['summary'])
+    except Exception as e:
+        print("Error fetching calendar:", e)
+
+
+fetch_calendar()
